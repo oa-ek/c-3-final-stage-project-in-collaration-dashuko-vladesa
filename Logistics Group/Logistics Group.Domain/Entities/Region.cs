f55cfa02bd36ff9135c1d04f0e807ff2c938ab80
daryna698 <darinailcuk698@gmail.com>
2026-03-22 20:32:14 +0200

    Add Geography and Infrastructure entities (Region, City, Branch)

---
+++ Logistics Group/Logistics Group.Domain/Entities/Region.cs
@@ -0,0 +1,10 @@
+﻿namespace LogisticsGroup.Domain.Entities
+{
+    public class Region
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<City> Cities { get; set; } = new List<City>();
+    }
+}
