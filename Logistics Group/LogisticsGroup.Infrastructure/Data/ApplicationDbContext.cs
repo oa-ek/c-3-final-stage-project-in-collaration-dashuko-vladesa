d76174a11df6fc532fa7d69267eefc63b5381bcc
daryna698 <darinailcuk698@gmail.com>
2026-03-22 16:10:57 +0200

    Configured database connection and registered DbContext

---
+++ Logistics Group/LogisticsGroup.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LogisticsGroup.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
