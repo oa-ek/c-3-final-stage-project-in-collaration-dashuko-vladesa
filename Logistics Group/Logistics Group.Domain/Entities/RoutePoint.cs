17ad4c8c188d8806c78e105ee4ddfb56fd37fd5f
daryna698 <darinailcuk698@gmail.com>
2026-03-22 20:33:47 +0200

    Add Logistics Routes and Transport Orders entities

---
+++ Logistics Group/Logistics Group.Domain/Entities/RoutePoint.cs
@@ -0,0 +1,15 @@
+﻿namespace LogisticsGroup.Domain.Entities
+{
+    public class RoutePoint
+    {
+        public int Id { get; set; }
+        public int Sequence { get; set; } 
+        public string OperationType { get; set; } = string.Empty; 
+
+        public int RouteId { get; set; }
+        public Route Route { get; set; } = null!;
+
+        public int BranchId { get; set; }
+        public Branch Branch { get; set; } = null!;
+    }
+}
---
+++ Logistics Group/Logistics Group.Domain/Entities/Trip.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LogisticsGroup.Domain.Entities
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+        public DateTime LoadingTime { get; set; }
+        public DateTime UnloadingTime { get; set; }
+
+        public int TransportOrderId { get; set; }
+        public TransportOrder TransportOrder { get; set; } = null!;
+
+        public int DepartureBranchId { get; set; }
+        public Branch DepartureBranch { get; set; } = null!;
+
+        public int ArrivalBranchId { get; set; }
+        public Branch ArrivalBranch { get; set; } = null!;
+    }
+}
