b85149e601bd5d9f7afd1b41439cbc61c7dcb244
daryna698 <darinailcuk698@gmail.com>
2026-03-22 20:33:09 +0200

    Add Cargo and Parcel entities

---
+++ Logistics Group/Logistics Group.Domain/Entities/Parcel.cs
@@ -0,0 +1,19 @@
+﻿namespace LogisticsGroup.Domain.Entities
+{
+    public class Parcel
+    {
+        public int Id { get; set; }
+        public string Barcode { get; set; } = string.Empty; 
+        public decimal Weight { get; set; }
+        public string Status { get; set; } = string.Empty; 
+
+        public int CategoryId { get; set; }
+        public CargoCategory Category { get; set; } = null!;
+
+        public int SenderBranchId { get; set; }
+        public Branch SenderBranch { get; set; } = null!;
+
+        public int ReceiverBranchId { get; set; }
+        public Branch ReceiverBranch { get; set; } = null!;
+    }
+}
