5fc71fa4a4daae9e47e84f36cb9314c76c37d0a6
daryna698 <darinailcuk698@gmail.com>
2026-03-22 15:45:38 +0200

    Setup Clean Architecture: added projects, references and EF Core packages

---
+++ Logistics Group/LogisticsGroup.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LogisticsGroup.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
