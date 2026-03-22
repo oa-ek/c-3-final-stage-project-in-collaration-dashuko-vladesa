f7e63b2bea430cdc89eacde585d04b3f1ec862ba
daryna698 <darinailcuk698@gmail.com>
2026-03-22 20:32:46 +0200

    Add Transport and Personnel entities (Vehicle, Driver)

---
+++ Logistics Group/Logistics Group.Domain/Entities/Vehicle.cs
@@ -0,0 +1,11 @@
+﻿namespace LogisticsGroup.Domain.Entities
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string LicensePlate { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public decimal Capacity { get; set; } 
+        public string Status { get; set; } = string.Empty; 
+    }
+}
