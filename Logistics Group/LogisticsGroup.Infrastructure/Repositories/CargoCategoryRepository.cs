8047d8d5912caae5543dc13973bdfe1b18fe4ec0
daryna698 <darinailcuk698@gmail.com>
2026-03-22 22:25:45 +0200

    Add Repository and Unit of Work patterns for CargoCategory

---
+++ Logistics Group/LogisticsGroup.Infrastructure/Repositories/CargoCategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using LogisticsGroup.Domain.Entities;
+using LogisticsGroup.Domain.Interfaces;
+using LogisticsGroup.Infrastructure.Data;
+using System.Linq;
+
+namespace LogisticsGroup.Infrastructure.Repositories
+{
+    public class CargoCategoryRepository : Repository<CargoCategory>, ICargoCategoryRepository
+    {
+        public CargoCategoryRepository(ApplicationDbContext ctx) : base(ctx)
+        {
+        }
+
+        public void Update(CargoCategory entity)
+        {
+            var categoryFromDb = _ctx.CargoCategories.FirstOrDefault(x => x.Id == entity.Id);
+
+            if (categoryFromDb is not null)
+            {
+                categoryFromDb.Name = entity.Name;
+            }
+        }
+    }
+}
--- Logistics Group/LogisticsGroup.Web/Program.cs
+++ Logistics Group/LogisticsGroup.Web/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<LogisticsGroup.Domain.Interfaces.IUnitOfWork, LogisticsGroup.Infrastructure.Repositories.UnitOfWork>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
