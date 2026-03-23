c4e1564b328a0b59938a0e99bd81556789236ad4
vladesa <vladyslava.pastushenko@oa.edu.ua>
2026-03-23 19:16:40 +0200

    Add Region controller and views

---
+++ Logistics Group/LogisticsGroup.Web/Controllers/RegionController.cs
@@ -0,0 +1,84 @@
+﻿using LogisticsGroup.Domain.Entities;
+using LogisticsGroup.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogisticsGroup.Web.Controllers
+{
+    public class RegionController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RegionController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // Список областей
+        public IActionResult Index()
+        {
+            var regionList = _unitOfWork.Region.GetAll();
+            return View(regionList);
+        }
+
+        // Додавання області
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Region obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Region.Add(obj);
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // Редагування області
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+            var regionFromDb = _unitOfWork.Region.Get(u => u.Id == id);
+            if (regionFromDb == null) return NotFound();
+            return View(regionFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Region obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Region.Update(obj);
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // Видалення області
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+            var regionFromDb = _unitOfWork.Region.Get(u => u.Id == id);
+            if (regionFromDb == null) return NotFound();
+            return View(regionFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _unitOfWork.Region.Get(u => u.Id == id);
+            if (obj == null) return NotFound();
+            _unitOfWork.Region.Remove(obj);
+            _unitOfWork.Save();
+            return RedirectToAction("Index");
+        }
+    }
+}
