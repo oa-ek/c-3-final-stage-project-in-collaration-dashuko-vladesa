e14b30502a7c72eaf9950dac31c704cda10ab246
vladesa <vladyslava.pastushenko@oa.edu.ua>
2026-04-10 23:29:05 +0300

    feat: added premium UI for Home and Logistician dashboard

---
+++ Logistics Group/LogisticsGroup.Web/Controllers/LogisticianController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LogisticsGroup.Web.Controllers
+{
+    public class LogisticianController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
